a15c77b67cf591fbe0a7a6433c2577c5d3ade575
jdiperi88 <jdiperi88@yahoo.com>
2018-12-11 21:58:53 -0500

    setup a counter app with 6 tests that all failed first and setup to pass using tdd

--- src/App.js
+++ src/App.js
@@ -3,10 +3,44 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+	state = {
+		counter: 0,
+		error: false
+	};
+
 	render() {
 		return (
-			<div className="App">
-				<h1>hello</h1>
+			<div data-test="component-app">
+				<h1 data-test="counter-display">{this.state.counter}</h1>
+				{this.state.error && (
+					<h1 style={{ color: "red" }}>you can not go below 0</h1>
+				)}
+				<button
+					onClick={() => {
+						this.setState(prev => {
+							return { counter: prev.counter + 1, error: false };
+						});
+					}}
+					data-test="increment-button"
+				>
+					Increment Counter
+				</button>
+				<button
+					onClick={() => {
+						const { counter } = this.state;
+						if (counter === 0) {
+							this.setState({ error: true });
+						} else {
+							this.setState(prev => ({
+								counter: prev.counter - 1,
+								error: false
+							}));
+						}
+					}}
+					data-test="decrement-button"
+				>
+					decrement counter
+				</button>
 			</div>
 		);
 	}
